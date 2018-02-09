3ee7fd00da3fbb330c722bbcc6dbfb6fdcbe8acf
ZXQLLJ <1512510237@qq.com>
2018-02-09 16:33:57 +0800

    thread_pool

---
+++ rc_thread_pool.cpp
@@ -0,0 +1,292 @@
+#include "rc_thread_pool.h"
+#include <unistd.h>
+#include <algorithm>
+#include <iostream>
+
+RCThread::RCThread():ThreadName(""),ThreadID(0)/*,retCode=0*/{}
+
+void RCThread::Create(pthread_attr_t *attr,void *(*handler)(void*),void *arg)
+{
+	pthread_create(&ThreadID,attr,handler,arg);
+	ThreadName.append("default");
+}
+
+void RCThread::Join()
+{
+	pthread_join(ThreadID,NULL);
+}
+
+
+RCThread::~RCThread()
+{
+	if(ThreadID!=0)
+		Join();
+}
+
+
+
+
+
+RCThreadPool::RCThreadPool():m_InitNum(10),idlesize(10),busysize(0){}
+
+
+RCThreadPool::RCThreadPool(int maxsize):m_InitNum(maxsize),idlesize(maxsize),busysize(0){}
+
+void RCThreadPool::InitGuard()
+{
+	RCWorkerThread *temp=new RCWorkerThread(*(new RCThreadCond()),*(new RCThreadMutex()));
+	RCJob *temp_job=new RCJob((void*)this);
+	temp_job->handler=&GuardThread;
+	temp->setJob(*temp_job);
+	temp->Prepare();
+	temp->Start();
+}
+
+void *RCThreadPool::GuardThread(void *argv)
+{
+	RCThreadPool *pool=(RCThreadPool*)argv;
+	while(1)
+	{
+		sleep(5);
+		pool->BusyMutex.lock();
+		if(pool->BusyList.size())
+		{
+			if(pool->BusyList.front()->getState()!=RUNNING)
+			{
+				pool->AppendToIdleList(pool->BusyList.front());
+				pool->BusyList.pop_front();
+				pool->busysize--;
+			}
+		}
+		pool->BusyMutex.unlock();
+	}
+}
+
+int RCThreadPool::getIdleSize() 
+{
+	IdleMutex.lock();
+	int size=idlesize;
+	IdleMutex.unlock();
+	return size;
+}
+
+int RCThreadPool::getBusySize() 
+{
+	BusyMutex.lock();
+	int size=busysize;
+	BusyMutex.unlock();
+	return size;
+}
+
+void RCThreadPool::CreateThreadPool()
+{
+	for(int i=0;i<m_InitNum;i++)
+	{
+		RCThreadCond *temp_cond=new RCThreadCond();
+		RCThreadMutex *temp_mux=new RCThreadMutex();
+		RCWorkerThread *temp=new RCWorkerThread(*temp_cond,*temp_mux);
+		IdleMutex.lock();
+		IdleList.push_back(temp);
+		IdleMutex.unlock();
+		temp->Prepare();
+	}
+}
+
+void RCThreadPool::AppendToIdleList(RCWorkerThread* t)
+{
+	IdleMutex.lock();
+	if(std::find(IdleList.begin(),IdleList.end(),t)==IdleList.end())
+	{
+		IdleList.push_back(t);
+		t->Suspend();
+		idlesize++;
+	}
+	IdleMutex.unlock();
+}
+
+void RCThreadPool::MoveToBusyList(RCWorkerThread* t)
+{
+	BusyMutex.lock();
+	if(std::find(BusyList.begin(),BusyList.end(),t)==BusyList.end())
+	{
+		BusyList.push_back(t);
+		t->Start();
+		busysize++;
+	}
+	BusyMutex.unlock();
+}
+
+void RCThreadPool::AddJob(RCJob& this_job)
+{
+	IdleMutex.lock();
+	if(IdleList.size())
+	{
+		IdleList.front()->setJob(this_job);
+		MoveToBusyList(IdleList.front());
+		IdleList.pop_front();
+		idlesize--;
+	}
+	printf("job added\n");
+	IdleMutex.unlock();
+}
+
+
+
+
+
+
+
+
+RCThreadMutex::RCThreadMutex()
+{
+	pthread_mutex_init(&mutex,NULL);
+}
+
+RCThreadMutex::~RCThreadMutex()
+{
+  	pthread_mutex_destroy(&mutex);
+}
+
+void RCThreadMutex::lock()
+{
+	pthread_mutex_lock(&mutex);  
+}
+
+void RCThreadMutex::unlock()
+{
+	pthread_mutex_unlock(&mutex);  
+}
+
+
+RCThreadCond::RCThreadCond()
+{
+	pthread_cond_init(&cond,NULL);
+}
+
+RCThreadCond::~RCThreadCond()
+{
+	pthread_cond_destroy(&cond);
+}
+
+void RCThreadCond::signal()
+{
+	pthread_cond_signal(&cond); 
+}
+
+void RCThreadCond::wait(RCThreadMutex& mux)
+{
+	pthread_cond_wait(&cond, &mux.mutex);
+}
+
+
+
+RCWorkerThread::RCWorkerThread(){}
+
+RCWorkerThread::RCWorkerThread(RCThreadCond& cond,RCThreadMutex& mux):state(NOTSTART),job(NULL)
+{
+	this->_cond=&cond;
+	this->_mux=&mux;
+}
+
+void RCWorkerThread::setJob(RCJob& this_job)
+{
+	this->job=&this_job;
+}
+
+void RCWorkerThread::Prepare()
+{
+	Create(NULL,&Run,(void*)this);
+	state=PREPARING;
+}
+
+void RCWorkerThread::Start()
+{
+	_mux->lock();
+	if(state!=RUNNING)
+	{
+		_cond->signal();
+		state=RUNNING;
+	}
+	_mux->unlock();
+}
+
+void RCWorkerThread::Suspend()
+{
+	_mux->lock();
+	if(state==RUNNING)
+		state=SUSPEND;
+	_mux->unlock();
+}
+
+void *RCWorkerThread::Run(void *argv)
+{
+	RCWorkerThread *this_p=(RCWorkerThread*)argv;
+	while(1)
+	{
+		this_p->_mux->lock();
+		while(this_p->state!=RUNNING)
+		{
+			this_p->_cond->wait(*(this_p->_mux));
+		}
+		this_p->_mux->unlock();
+
+		if(this_p->job)
+			(*(this_p->job->handler))(this_p->job->Jobdata);
+
+		this_p->job=NULL;
+		this_p->Suspend();
+	}
+}
+
+int RCWorkerThread::getState()
+{
+	_mux->lock();
+	int temp_state=state;
+	_mux->unlock();
+	return temp_state;
+}
+
+RCWorkerThread::~RCWorkerThread()
+{
+	if(state==RUNNING)
+		Join();
+}
+
+
+
+
+
+
+
+
+RCJob::RCJob():JobName("default"),JobID(0),Jobdata(NULL){}
+
+RCJob::RCJob(void *jobdata)
+{
+	this->Jobdata=jobdata;
+}
+
+RCJob::~RCJob(){}
+
+
+
+
+
+
+RCThreadManager::RCThreadManager()
+{
+	pool.CreateThreadPool();
+	pool.InitGuard();
+}
+
+RCThreadManager::RCThreadManager(int maxsize):pool(maxsize)
+{
+	pool.CreateThreadPool();
+	pool.InitGuard();
+}
+
+void RCThreadManager::Run(RCJob& this_job)
+{
+	pool.AddJob(this_job);
+}
+
---
+++ rc_thread_pool.h
@@ -0,0 +1,160 @@
+#ifndef RC_THREAD_POOL_H
+#define RC_THREAD_POOL_H
+
+#include <pthread.h>
+#include <string>
+#include <string.h>
+#include <deque>
+#include <map>
+
+class RCThreadMutex;
+class RCJob;
+class RCThread;
+class RCWorkerThread;
+class RCThreadCond;
+class RCThreadPool;
+class RCThreadManager;
+
+
+class RCJob
+{
+public:
+	RCJob();
+	RCJob(void *jobdata);
+	virtual ~RCJob();
+
+	friend RCWorkerThread;
+
+	void setJobID(int id){ JobID=id; }
+	int getJobID() const { return JobID; }
+	void setJobName(std::string& name){ JobName=name; }
+	std::string getJobName() const { return JobName; }
+
+	void *(*handler)(void*);
+protected:
+	std::string JobName;
+	int JobID;
+	void *Jobdata;
+};
+
+class RCThreadMutex
+{
+public:
+	RCThreadMutex();
+    ~RCThreadMutex();
+
+    friend class RCThreadCond;
+
+    void lock();
+    void unlock();
+
+private:
+    pthread_mutex_t mutex;
+};
+
+class RCThreadCond
+{
+public:
+	RCThreadCond();
+	~RCThreadCond();
+
+	void signal();
+	void wait(RCThreadMutex& mux);
+private:
+	pthread_cond_t cond;  
+};
+
+class RCThread
+{
+public:
+	RCThread(); 
+	virtual ~RCThread();
+
+	void Join();
+	void Create(pthread_attr_t *attr,void *(*handler)(void*),void *arg);
+	inline pthread_t getThreadID() const{return ThreadID;}
+	inline void setThreadID(int id){this->ThreadID=id;}
+	//inline int getRetCode() const{return retCode;}
+	inline std::string getThreadName() const{return ThreadName;}
+	inline void setThreadName(std::string &str){this->ThreadName=str;}
+protected:
+	std::string ThreadName;
+	//int retCode;
+	pthread_t ThreadID;
+};
+
+
+#define NOTSTART 0
+#define RUNNING 1
+#define SUSPEND 2
+#define PREPARING 3
+
+class RCWorkerThread:public RCThread
+{
+public:
+	RCWorkerThread();
+	RCWorkerThread(RCThreadCond& cond,RCThreadMutex& mux);
+	~RCWorkerThread();
+
+	void setJob(RCJob& this_job);
+
+	void Prepare();
+	void Start();
+	void Suspend();
+	int getState();
+	//void *(*handler)(void*);
+	static void *Run(void *argv);
+
+private:
+	RCJob *job;
+	RCThreadMutex* _mux;
+	RCThreadCond* _cond;
+	int state;
+};
+
+class RCThreadPool
+{
+public:
+	RCThreadPool();
+	RCThreadPool(int maxsize);
+	~RCThreadPool(){};
+
+	void CreateThreadPool();
+	void AppendToIdleList(RCWorkerThread* t);
+	void MoveToBusyList(RCWorkerThread* t);
+
+	int getIdleSize();
+	int getBusySize();
+
+	void AddJob(RCJob& this_job);
+
+	void InitGuard();
+	static void *GuardThread(void *argv);
+private:
+	RCThreadMutex IdleMutex;
+	RCThreadMutex BusyMutex;
+	std::deque<RCWorkerThread*> IdleList;
+	std::deque<RCWorkerThread*> BusyList;
+	int m_InitNum;
+	int idlesize;
+	int busysize;
+};
+
+
+
+class RCThreadManager
+{
+public:
+	RCThreadManager();
+	RCThreadManager(int maxsize);
+	~RCThreadManager(){}
+
+	void Run(RCJob& this_job);
+private:
+	RCThreadPool pool;
+};
+
+
+
+
+#endif
